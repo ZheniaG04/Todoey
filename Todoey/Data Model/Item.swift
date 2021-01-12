6d04779913574865ed67a1208abe3b2873c39139
ZheniaG04 <evhenguts@gmail.com>
2021-01-13 01:30:41 +0200

    Code refactoring

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Женя on 13.01.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
