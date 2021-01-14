725f10ccd1ce37c76b635fc74a01d4f981638f62
ZheniaG04 <evhenguts@gmail.com>
2021-01-15 00:42:24 +0200

    Added item array encoding and decoding

--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -3,12 +3,11 @@
 //  Todoey
 //
 //  Created by Женя on 13.01.2021.
-//  Copyright © 2021 App Brewery. All rights reserved.
 //
 
 import Foundation
 
-class Item {
+class Item : Codable {
     var title : String = ""
     var done : Bool = false
 }
